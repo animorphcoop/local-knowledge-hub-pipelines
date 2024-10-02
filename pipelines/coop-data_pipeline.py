77715bfda2d4cd1a316d7c3023cfb9125090d08e
mario <mario@animorph.coop>
2024-10-02 15:23:13 +0100

    versioning example pipelines from the hackathon

---
+++ pipelines/coop-data_pipeline.py
@@ -0,0 +1,98 @@
+"""
+title: Llama Index Ollama Pipeline
+author: open-webui
+date: 2024-05-30
+version: 1.0
+license: MIT
+description: A pipeline for retrieving relevant information from a knowledge base using the Llama Index library with Ollama embeddings.
+# requirements: llama-index, llama-index-llms-ollama, llama-index-embeddings-ollama
+"""
+
+from typing import List, Union, Generator, Iterator
+from schemas import OpenAIChatMessage
+import os
+import logging 
+import sys
+
+from pydantic import BaseModel
+from llama_index.core import StorageContext, load_index_from_storage
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+
+
+class Pipeline:
+
+    class Valves(BaseModel):
+        LLAMAINDEX_OLLAMA_BASE_URL: str
+        LLAMAINDEX_MODEL_NAME: str
+        LLAMAINDEX_EMBEDDING_MODEL_NAME: str
+
+    def __init__(self):
+        self.documents = None
+        self.index = None
+
+        self.valves = self.Valves(
+            **{
+                "LLAMAINDEX_OLLAMA_BASE_URL": os.getenv("LLAMAINDEX_OLLAMA_BASE_URL", "http://localhost:11434"),
+                "LLAMAINDEX_MODEL_NAME": os.getenv("LLAMAINDEX_MODEL_NAME", "mistral-nemo"),
+                "LLAMAINDEX_EMBEDDING_MODEL_NAME": os.getenv("LLAMAINDEX_EMBEDDING_MODEL_NAME", "nomic-embed-text"),
+            }
+        )
+
+    async def on_startup(self):
+        from llama_index.embeddings.ollama import OllamaEmbedding
+        from llama_index.llms.ollama import Ollama
+        from llama_index.core import Settings, VectorStoreIndex, SimpleDirectoryReader
+
+        Settings.embed_model = OllamaEmbedding(
+            model_name=self.valves.LLAMAINDEX_EMBEDDING_MODEL_NAME,
+            base_url=self.valves.LLAMAINDEX_OLLAMA_BASE_URL,
+        )
+        Settings.llm = Ollama(
+            model=self.valves.LLAMAINDEX_MODEL_NAME,
+            base_url=self.valves.LLAMAINDEX_OLLAMA_BASE_URL,
+        )
+
+        # This function is called when the server is started.
+        global documents, index
+        
+        #print('loading data dir with SimpleDirectoryReader')
+        #self.documents = SimpleDirectoryReader(input_files=["data/worldwide_coop_data.csv"]).load_data()
+        #print('loaded finished')
+        #print('building index from docs')
+        #self.index = VectorStoreIndex.from_documents(self.documents)	
+        #print('index built')
+        #print('storing index')
+        ##self.index.storage_context.persist(persist_dir='./indexes/')
+        #print('index stored')
+        
+        print('loading stored index')
+        storage_context = StorageContext.from_defaults(persist_dir='indexes/coop-data-pipeline')
+        self.index = load_index_from_storage(storage_context)
+        
+        pass
+
+    async def on_shutdown(self):
+        # This function is called when the server is stopped.
+        pass
+
+    def pipe(
+        self, user_message: str, model_id: str, messages: List[dict], body: dict
+    ) -> Union[str, Generator, Iterator]:
+        # This is where you can add your custom RAG pipeline.
+        # Typically, you would retrieve relevant information from your knowledge base and synthesize it to generate a response.
+
+        print(messages)
+        print(user_message)
+        
+        print('building query engine from index')
+        query_engine = self.index.as_query_engine(streaming=True)
+        #chat_engine = self.index.as_chat_engine()
+        print('query engine built')
+        print('building response from query engine and user prompt')
+        response = query_engine.query(user_message)
+        #response = chat_engine.chat(user_message)
+        print('response built')
+
+        return response.response_gen
